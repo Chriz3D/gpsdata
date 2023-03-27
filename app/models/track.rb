9db15ac15696e8109498d42ad412f9c16c32acf3
Chriz3D <zeppelin@inode.at>
2023-03-27 15:36:31 +0200

    parse time

--- app/models/track.rb
+++ app/models/track.rb
@@ -3,7 +3,15 @@ class Track < ApplicationRecord
   validates :name, presence: true
   
   def parseGpx
-  
+     doc = Nokogiri::XML(open("public/" + self.attachment_url))
+     #trackpoints = doc.xpath('//xmlns:trkpt')
+     starttime = doc.xpath('//xmlns:time')
+     
+     if(starttime != nil && starttime.length > 0)
+        return starttime[0].text
+     else 
+        return ""
+     end   
   end
     
 end
